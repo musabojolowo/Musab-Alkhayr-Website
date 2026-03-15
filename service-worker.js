94a364251b0c044af7d5b174399c056e63c2958f
Mu <musabojolowo2006@gmail.com>
2026-03-15 08:23:16 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "musab-pwa-v2";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/about.html",
+  "/services.html",
+  "/contact.html",
+  "/offline.html",
+  "/style.css",
+  "/script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request).catch(() => {
+          return caches.match("/offline.html");
+        });
+      })
+  );
+});
